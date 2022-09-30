8eede1954bde4eea2434a38165b45dd39fa8220e
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-30 13:28:08 +0300

    Update UpdateChannel.py
    Signed-off-by: Ramo <112237128+ramoben200@users.noreply.github.com>

--- UpdateChannel.py
+++ UpdateChannel.py
@@ -9,7 +9,7 @@ async def Subscribe(lel, message):
             await app.send_message(chat_id=message.chat.id,text=f"Üzgünüm efendim, Yasaklandınız. İletişim Support.", parse_mode="markdown", disable_web_page_preview=True)
             return 1
       except UserNotParticipant:
-await app.send_message(chat_id=message.chat.id, text="Lütfen Beni Kullanmak İçin Güncellemelerim Kanalına Katılın!\n ve Kontrol etmek için tıklayın /start", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 Güncellemeler Kanalına Katılın 🤖", url=f"https://t.me/{JasmineSup}")]]), parse_mode="markdown")
+await app.send_message(chat_id=message.chat.id, text="Lütfen Beni Kullanmak İçin Güncellemelerim Kanalına Katılın!\n ve Kontrol etmek için tıklayın /start", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 Güncellemeler Kanalına Katılın 🤖", url=f"https://t.me/{Ballasresmi}")]]), parse_mode="markdown")
          return 1
       except Exception:
          await app.send_message(chat_id=message.chat.id, text=f"Bir şeyler yanlış gitti. İletişim Support.", parse_mode="markdown", disable_web_page_preview=True)
